6f2afb26636213339ee33a0aa9d38a5820d59544
andrewr1616 <andrewreese1616@gmail.com>
2024-09-09 16:50:28 -0400

    completed all js and ejs

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ dotenv.config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const path = require("path");
 const CarModel = require("./models/car");
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -12,9 +15,31 @@ mongoose.connection.on("connected", function () {
 
 //Midleware
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/cars/:carId/edit", async function (req, res) {
+  const carDoc = await CarModel.findById(req.params.carId, req.body);
+  res.render("cars/edit.ejs", { car: carDoc });
+});
+app.put("/cars/:carId", async function (req, res) {
+  await CarModel.findByIdAndUpdate(req.params.carId, req.body);
+  res.redirect(`/cars/${req.params.carId}`);
+});
+
+app.delete("/cars/:carId", async function (req, res) {
+  const deletedCar = await CarModel.findByIdAndDelete(req.params.carId);
+  res.redirect("/cars");
+});
 
 app.get("/cars/new", async function (req, res) {
   res.render("cars/new.ejs");
+});
+
+app.get("/cars/:carId", async function (req, res) {
+  const carDoc = await CarModel.findById(req.params.carId);
+  res.render("cars/show.ejs", { car: carDoc });
 });
 
 app.get("/cars", async function (req, res) {
@@ -28,7 +53,7 @@ app.post("/cars", async function (req, res) {
 
   const createdCar = await CarModel.create(req.body);
   console.log(createdCar);
-  res.redirect("/cars/new");
+  res.redirect("/cars");
 });
 
 app.get("/", function (req, res) {
